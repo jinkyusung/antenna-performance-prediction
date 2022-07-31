de12bee2bfd5a8b7066f5359a7861d2216af77d9
Jinkyu Sung <sungjk1999@gmail.com>
2022-07-31 14:33:18 +0900

    [Update] `modeling.py`
    **Define:** `maxminError`, `meanError`, `keywordError`.
    **Modify:** function `NRMSE()` use above three errors.

--- mylibrary/modeling.py
+++ mylibrary/modeling.py
@@ -1,30 +1,70 @@
+from msilib.schema import Error
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
 
+######################### python Error classes ##############################
+class maxminError(Exception):
+    def __init__(self):
+        self.msg = 'maxminError: max(y_true) is greater than min(y_true)'
+    
+    def __str__(self):
+        return self.msg
+
+
+class meanError(Exception):
+    def __init__(self, msg='meanError: mean(y_true) is not Zero'):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+
+
+class keywordError(Exception):
+    def __init__(self, user_input=""):
+        self.msg = "keywordError: invalid keyword for NRMSE()"
+        self.msg += f"""
+                    divisor should be 'max-min' or 'mean'
+                    your input is '{user_input}'.
+                    """
+    
+    def __str__(self):
+        return self.msg
+#############################################################################
+
+
+
+
+################################ Functions ##################################
 def RMSE(y_true, y_pred):
+    """Calculate the Root Mean Squared Error."""
     return mean_squared_error(y_true, y_pred) ** 0.5
 
 
 def NRMSE(y_true, y_pred, divisor='max-min', percentage=True):
     """Calculate the Normalized Root Mean Squared Error."""
-    
-    result = RMSE(y_true, y_pred)
-    if divisor == 'max-min':
-        max_y_true = max(y_true)
-        min_y_true = min(y_true)
-        if max_y_true <= min_y_true:
-            raise "ValueError: divisor is not Zero."
+    try:
+        result = RMSE(y_true, y_pred)
+        if divisor == 'max-min':
+            max_y_true = max(y_true)
+            min_y_true = min(y_true)
+            if max_y_true <= min_y_true:
+                raise maxminError()
+            else:
+                result /= max_y_true - min_y_true
+        elif divisor == 'mean':
+            result /= np.mean(y_true)
         else:
-            result /= max_y_true - min_y_true
+            raise keywordError(divisor)
 
-    elif divisor == 'mean':
-        result /= np.mean(y_true)
-
-    else:
-        raise "divisor should be 'max-min' or 'mean'."
-
-    if percentage:
-        result *= 100
-    return result
-
+        if percentage:
+            result *= 100
+        return result
+    
+    except maxminError:
+        print(maxminError)
+    
+    except Exception:
+        import traceback
+        traceback.print_exc()
+#############################################################################

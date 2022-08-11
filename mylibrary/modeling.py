94c5ec2e740be26245f7e5c7b5a3c1bc5cd3c179
Jinkyu Sung <sungjk1999@gmail.com>
2022-08-11 13:22:50 +0900

    [Update] `modeling.py`

--- mylibrary/modeling.py
+++ mylibrary/modeling.py
@@ -4,32 +4,13 @@ import numpy as np
 
 
 ######################### python Error classes ##############################
-class maxminError(Exception):
-    def __init__(self):
-        self.msg = 'maxminError: max(y_true) is greater than min(y_true)'
-    
-    def __str__(self):
-        return self.msg
-
-
 class meanError(Exception):
     def __init__(self, msg='meanError: mean(y_true) is not Zero'):
         self.msg = msg
     
     def __str__(self):
         return self.msg
-
-
-class keywordError(Exception):
-    def __init__(self, user_input=""):
-        self.msg = "keywordError: invalid keyword for NRMSE()"
-        self.msg += f"""divisor should be 'max-min' or 'mean
-                        your input is '{user_input}'."""
-    
-    def __str__(self):
-        return self.msg
 #############################################################################
-
 
 
 
@@ -38,31 +19,15 @@ def RMSE(y_true, y_pred):
     """Calculate the Root Mean Squared Error."""
     return mean_squared_error(y_true, y_pred) ** 0.5
 
-
-def NRMSE(y_true, y_pred, divisor='max-min', percentage=True):
-    """Calculate the Normalized Root Mean Squared Error."""
-    try:
-        result = RMSE(y_true, y_pred)
-        if divisor == 'max-min':
-            max_y_true = max(y_true)
-            min_y_true = min(y_true)
-            if max_y_true <= min_y_true:
-                raise maxminError()
-            else:
-                result /= max_y_true - min_y_true
-        elif divisor == 'mean':
-            result /= np.mean(y_true)
-        else:
-            raise keywordError(divisor)
-
-        if percentage:
-            result *= 100
-        return result
-    
-    except maxminError:
-        print(maxminError)
-    
-    except Exception:
-        import traceback
-        traceback.print_exc()
+        
+def lg_nrmse(gt, preds):
+    # 각 Y Feature별 NRMSE 총합
+    # Y_01 ~ Y_08 까지 20% 가중치 부여
+    all_nrmse = []
+    for idx in range(1,15): # ignore 'ID'
+        rmse = mean_squared_error(gt[:,idx], preds[:,idx], squared=False)
+        nrmse = rmse/np.mean(np.abs(gt[:,idx]))
+        all_nrmse.append(nrmse)
+    score = 1.2 * np.sum(all_nrmse[:8]) + 1.0 * np.sum(all_nrmse[8:14])
+    return score
 #############################################################################

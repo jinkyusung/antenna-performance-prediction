a61d89fd5c83a8bf047000263d239cc895b0da09
Jinkyu Sung <sungjk1999@gmail.com>
2022-08-17 18:58:45 +0900

    [Update] Change func `pred_visualize` to `show_pred`

--- mylibrary/visualization.py
+++ mylibrary/visualization.py
@@ -25,7 +25,7 @@ def show_plot(df, mean=True, median=False, horizons=[], \
     plt.show()
 
 
-def pred_visualize(train, pred):
+def show_pred(train, pred):
     pred_t = list(zip(*pred))   # Transposed matrix of pred.
     n_feature = len(pred_t)     # The number of feature
     n_pred = len(pred)          # The number of prediction
@@ -41,6 +41,26 @@ def pred_visualize(train, pred):
         ax.scatter(train_idx, train.iloc[:, i], s=0.5, c='gray')
         ax.scatter(pred_idx, pred_t[i], s=0.5, c='coral')
     plt.show()
+
+
+def show_pred_df(train, submission):
+    n_feature = len(submission.columns)     # The number of feature
+    n_pred = len(submission)          # The number of prediction
+    
+    train_idx = train.index.to_list()
+    pred_idx = [k + train_idx[-1] for k in range(1, n_pred + 1)]
+
+    fig = plt.figure(figsize=(30, 5 * n_feature))
+    
+    for i in range(n_feature):
+        ax = fig.add_subplot(n_feature, 1, i+1)
+        ax.set_title(f'{train.columns[i]}', fontdict={'fontsize': 16,'fontweight':'bold'})
+        ax.scatter(train_idx, train.iloc[:, i], s=0.5, c='gray')
+        ax.scatter(pred_idx, submission.iloc[:, i+1], s=0.5, c='coral')
+    plt.show()
+
+
+
     
     
 def print_namegroup(namegroup):
